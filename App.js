1cc3ebc413fb2b74b80410ab158cc4f658c72660
Grace Kim <55515600+gracek904@users.noreply.github.com>
2019-11-16 22:30:58 -0600

    Add files via upload
    can't use git command somehow :(

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
 import Routes from "./Route";
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCgPTlv7q0nLT8JKqcLyn_sve8RRkPvceA",
+  projectId: "nutrition-b17ba"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+global.db = db;
 
 export default class App extends Component {
   render() {
