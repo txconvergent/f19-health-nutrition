5e286c539844942849186bf92fa2bd36b90d7ef2
shahzadsb31 <55596816+shahzadsb31@users.noreply.github.com>
2019-12-01 15:43:02 -0600

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,770 @@
+const tejis = [
+  {
+    id: 9,
+    title: "Palak Aloo - 350 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 10,
+    title: "Aloo Matar - 460 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 1,
+    title: "Chana Makhni Masala - 700 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 2,
+    title: "Stuffed Baingan - 410 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 11,
+    title: "Bhindi Masala - 400 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 12,
+    title: "Vegetable Jalfrezi - 430 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 13,
+    title: "Dum Aloo - 410 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 3,
+    title: "Matar Paneer - 460 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 6,
+    title: "Veggie Wrap - 860 calories",
+    caption: "Recommended - Vegan, No Nut", 
+    rec: true
+  },  {
+    id: 7,
+    title: "Yellow Dal - 440 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 4,
+    title: "Aloo Gobhi - 410 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 5,
+    title: "Baingan Bharta - 390 calories",
+    caption: "Recommended - Vegan, No Nut", 
+    rec: true
+  }, {
+    id: 8,
+    title: "Samosa - 250 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 14,
+    title: "Lamb Vindaloo - 740 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 15,
+    title: "Paneer Tikka - 690 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 16,
+    title: "Dal Makhni - 670 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 17,
+    title: "Lamb Curry - 890 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 18,
+    title: "Kadahi Lamb - 660 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 19,
+    title: "Lamb Tikka Masala - 910 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 20,
+    title: "Lamb Kofta - 660 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 21,
+    title: "Goat Vindaloo - 760 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 22,
+    title: "Goat Saag - 810 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 23,
+    title: "Chicken Lollipop - 600 calories",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 24,
+    title: "Malai Kofta - 620 calories",
+    caption: "Non-Vegan, No Nut"
+  },  {
+    id: 25,
+    title: "Egg Curry - 530 calories",
+    caption: "Non-Vegan, No Nut"
+  },  {
+    id: 26,
+    title: "Gulab Jamun - 380 calories",
+    caption: "Non-Vegan, No Nut"
+  },  {
+    id: 27,
+    title: "Ras Malai - 400 calories",
+    caption: "Non-Vegan, Nut"
+  },  {
+    id: 28,
+    title: "Kheer - 300 calories",
+    caption: "Non-Vegan, Nut"
+  },  {
+    id: 29,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },  {
+    id: 30,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 31,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 32,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 33,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 34,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 35,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 36,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 37,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 38,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 39,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 40,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 41,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 42,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 43,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 44,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+
+];
+
+const foo = [
+  {
+    id: 11,
+    title: "Bhindi Masala - 400 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  },
+  {
+    id: 12,
+    title: "Vegetable Jalfrezi - 430 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  },
+  {
+    id: 13,
+    title: "Dum Aloo - 410 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  },
+  {
+    id: 3,
+    title: "Matar Paneer - 460 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  }, {
+    id: 9,
+    title: "Palak Aloo - 350 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  },
+  {
+    id: 10,
+    title: "Aloo Matar - 460 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  },
+  {
+    id: 1,
+    title: "Chana Makhni Masala - 700 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  },{
+    id: 6,
+    title: "Veggie Wrap - 860 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal", 
+    rec: true
+  },  {
+    id: 7,
+    title: "Yellow Dal - 400 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  }, {
+    id: 8,
+    title: "Samosa - 250 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  },
+  {
+    id: 2,
+    title: "Stuffed Baingan - 410 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  },{
+    id: 4,
+    title: "Aloo Gobhi - 410 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal",
+    rec: true
+  }, {
+    id: 5,
+    title: "Baingan Bharta - 390 calories",
+    caption: "Recommended - Vegetarian, Lactose Intolerant, Halal", 
+    rec: true
+  }, 
+  {
+    id: 14,
+    title: "Lamb Vindaloo - 740 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 15,
+    title: "Paneer Tikka - 690 calories",
+    caption: "Non-Vegan"
+  },
+  
+  {
+    id: 16,
+    title: "Dal Makhni - 670 calories",
+    caption: "Non-Vegan"
+  },
+  
+  {
+    id: 17,
+    title: "Lamb Curry - 890 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 18,
+    title: "Kadahi Lamb - 660 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 19,
+    title: "Lamb Tikka Masala - 910 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 20,
+    title: "Lamb Kofta - 660 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 21,
+    title: "Goat Vindaloo - 760 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 22,
+    title: "Goat Saag - 810 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 23,
+    title: "Chicken Lollipop - 600 calories",
+    caption: "Non-Vegan, Nut"
+  },
+ {
+    id: 24,
+    title: "Malai Kofta - 620 calories",
+    caption: "Non-Vegan, No Nut"
+  },  {
+    id: 25,
+    title: "Egg Curry - 530 calories",
+    caption: "Non-Vegan, No Nut"
+  },   {
+    id: 26,
+    title: "Gulab Jamun - 380 calories",
+    caption: "Non-Vegan, No Nut"
+  },  {
+    id: 27,
+    title: "Ras Malai - 400 calories",
+    caption: "Non-Vegan, Nut"
+  },  {
+    id: 28,
+    title: "Kheer - 300 calories",
+    caption: "Non-Vegan, Nut"
+  },  {
+    id: 29,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },  {
+    id: 30,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 31,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 32,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 33,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 34,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 35,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 36,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 37,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 38,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 39,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 40,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 41,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 42,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 43,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+  {
+    id: 44,
+    title: "Peshawari Nan",
+    caption: "Non-Vegan, Nut"
+  },
+];
+
+const madams = [
+  {
+    id: 1,
+    title: "Sen Lek Hang",
+    caption: "Recommended - Vegetarian Option: Tofu",
+    rec: true
+  },
+  {
+    id: 2,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegetarian, Non-Halal"
+  },
+  {
+    id: 3,
+    title: "Pad Thai",
+    caption: "Recommended - Vegetarian Option: Tofu",
+    rec: true
+  },
+  {
+    id: 4,
+    title: "Lard Na",
+    caption: "Non-Vegetarian"
+  },
+  {
+    id: 5,
+    title: "Tom Yum",
+    caption: "Recommended - Vegetarian Option: Tofu",
+    rec: true
+  },
+  {
+    id: 6,
+    title: "Rice Soup",
+    caption: "Non-Lactose Intolerant"
+  },
+  {
+    id: 7,
+    title: "Sticky Rice With Mango",
+    caption: "Non-Lactose Intolerant"
+  }
+];
+
+// change this
+const madam = [
+  {
+    id: 1,
+    title: "Sen Lek Hang - 850 calories",
+    caption: "Recommended - Vegan Option: Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 2,
+    title: "Pad Kee Mao - 880 calories",
+    caption: "Recommended - Vegan Option : Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 3,
+    title: "Pad Thai - 960 calories ",
+    caption: "Recommended - Vegan Option: Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 4,
+    title: "Pad Ga-Tiam Prik Thai - 810 calories",
+    caption: "Recommended - Vegan Option: Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 5,
+    title: "Tom Yum - 450 calories",
+    caption: "Recommended - Vegan Option: Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 6,
+    title: "Rice Soup - 440 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 7,
+    title: "Sticky Rice With Mango - 500 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true
+  },
+  {
+    id: 8,
+    title: "Yen Ta Fo - 810 calories",
+    caption: "Recommended - Vegan Option : Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 9,
+    title: "Tom Khlong - 500 calories",
+    caption: "Recommended - Vegan Option : Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 10,
+    title: "Pad Ped Ga-Prao - 890 calories",
+    caption: "Recommended - Vegan Option : Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 11,
+    title: "Vegetable Stir-fried - 720 calories",
+    caption: "Recommended - Vegan, No Nut",
+    rec: true 
+  },
+  {
+    id: 12,
+    title: "Kale Stir-fried - 700 calories",
+    caption: "Recommended - Vegan Option : Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 13,
+    title: "Pad Prik Thai Dum - 740 calories",
+    caption: "Recommended - Vegan Option : Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 14,
+    title: "Gang Jeud - 590 calories",
+    caption: "Recommended - Vegan Option : Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 15,
+    title: "Thai Sweet and Sour - 970 calories",
+    caption: "Recommended - Vegan Option : Tofu, No Nut",
+    rec: true
+  },
+  {
+    id: 16,
+    title: "Guay Teaw Nuer Pue - 760 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 17,
+    title: "Pad Macaroni - 830 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 18,
+    title: "Guay Teaw Kua Gai - 700 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 19,
+    title: "Garee Curry - 650 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 20,
+    title: "Keow Wan Curry - 630 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 21,
+    title: "Gang Kua Supparod - 710 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 22,
+    title: "Panang Curry - 730 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 23,
+    title: "Gang Pa - 600 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 24,
+    title: "Fried Rice - 610 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 25,
+    title: "Crab Fried Rice - 720 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 26,
+    title: "Street Fried Rice - 680 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 27,
+    title: "Pineapple Fried Rice - 630 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 28,
+    title: "Ga-Prao Kai Daow - 840 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 29,
+    title: "Tom Kha - 390 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 30,
+    title: "Amazing Squid or Shrimp - 630 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 31,
+    title: "Kai Jeaw - 450 calories",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 32,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 33,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 34,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 35,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 36,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 37,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 38,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 39,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 40,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 41,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 42,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 43,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 44,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 45,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 46,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 47,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 48,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 49,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  },
+  {
+    id: 50,
+    title: "Guay Teaw Nuer Puey",
+    caption: "Non-Vegan"
+  }
+];
+
+export default {
+  tejis,
+  foo,
+  madam
+};
